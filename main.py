12057c3516d4405fc1750147a1fb303a530f4ed8
Dave Hudson <dave-hudson@users.noreply.github.com>
2022-11-13 22:04:59 +0000

    Experiment with some graphical elements

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
 import sys
+import json 
 
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QAction, QIcon
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QStatusBar, QToolBar
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QStatusBar, QToolBar, QListWidget, QSplitter
+
+def load_json(json_file_path):
+    with open(json_file_path, encoding = 'utf-8') as user_file: 
+        json_data = json.load(user_file)
+        return json_data
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,7 +19,22 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 
+json_data = load_json("people.json")
+list_widget = QListWidget()
+
+all_people_list = json_data["People"]
+for i in all_people_list:
+    list_widget.addItem(i["Name"])
+
+text_widget = QListWidget()
+text_widget.addItem("foo")
+
+splitter_widget = QSplitter()
+splitter_widget.addWidget(list_widget)
+splitter_widget.addWidget(text_widget)
+
 window = MainWindow()
+window.setCentralWidget(splitter_widget)
 window.show()
 
 app.exec()
