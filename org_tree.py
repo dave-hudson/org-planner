ebba24ced599c7b06ee0356929b03f533c6230f9
Dave Hudson <dave-hudson@users.noreply.github.com>
2023-01-02 21:51:43 +0000

    Calculate roll-ups of direct reports.

--- org_tree.py
+++ org_tree.py
@@ -29,11 +29,14 @@ def scan_json(json_data):
     return (failed, people, top_level, direct_reports)
 
 def scan_org_tree(people, supervisor_uen, direct_reports, depth):
+    num_reports = 0
     if supervisor_uen in direct_reports.keys():
         for i in direct_reports[supervisor_uen]:
-            scan_org_tree(people, i, direct_reports, depth + 1)
+            num_reports += scan_org_tree(people, i, direct_reports, depth + 1) + 1
 
     people[supervisor_uen]["Org Depth"] = depth
+    people[supervisor_uen]["Total Reports"] = num_reports
+    return num_reports
 
 json_file_path = r'people.json'
 with open(json_file_path, encoding = 'utf-8') as user_file: 
@@ -50,7 +53,7 @@ print(level_count)
 for i in all_people:
     p = all_people[i]
     depth = p["Org Depth"]
-    print(p["Person"]["UEN"], ":", p["Org Depth"])
+    print(p["Person"]["UEN"], ":", p["Org Depth"], ":", p["Total Reports"])
     level_count[depth] += 1
 
 for i in range(len(level_count)):
