d6eeb2e145736bdd6b1b6a25a5406886f72b8e6c
Dave Hudson <dave-hudson@users.noreply.github.com>
2023-01-03 18:30:59 +0000

    Add some GUI elements
    * Remove the unecessary direct_reports dictionary.
    
    * Scan the tree depth when rendering.
    
    * Add sprial rendering of the org.

--- org_tree.py
+++ org_tree.py
@@ -1,9 +1,9 @@
 import json
 import sys
+from PySide6 import QtCore, QtGui, QtWidgets
 
 def scan_json(json_data):
     people = {}
-    direct_reports = {}
     top_level = 0
     failed = False
 
@@ -12,7 +12,10 @@ def scan_json(json_data):
         uen = i["UEN"]
         people[uen] = {}
         people[uen]["Person"] = i
+        people[uen]["Direct Reports"] = []
 
+    for i in all_people_list:
+        uen = i["UEN"]
         if "Supervisor UEN" not in i.keys():
             if top_level == 0:
                 top_level = uen
@@ -21,50 +24,125 @@ def scan_json(json_data):
                 failed = True
         else:
             supervisor_uen = i["Supervisor UEN"]
-            if supervisor_uen not in direct_reports.keys():
-                direct_reports[supervisor_uen] = []
+            people[supervisor_uen]["Direct Reports"].append(uen)
 
-            direct_reports[supervisor_uen].append(uen)
+    return (failed, people, top_level)
 
-    return (failed, people, top_level, direct_reports)
-
-def scan_org_tree(people, supervisor_uen, direct_reports, depth):
+def scan_org_tree(people, supervisor_uen, depth):
     # Scan each direct report recursively, computing how deep each person is in
     # the overall org, and how many reports roll up to them in total.
     num_reports = 0
-    if supervisor_uen in direct_reports.keys():
-        for i in direct_reports[supervisor_uen]:
-            num_reports += scan_org_tree(people, i, direct_reports, depth + 1) + 1
+    p = people[supervisor_uen]
+    for i in p["Direct Reports"]:
+        num_reports += scan_org_tree(people, i, depth + 1) + 1
 
-    people[supervisor_uen]["Org Depth"] = depth
-    people[supervisor_uen]["Total Reports"] = num_reports
+    p["Org Depth"] = depth
+    p["Total Reports"] = num_reports
 
     # Scan each direct report, but this time compute the fraction of the overall
     # team their subteam represents.
-    if supervisor_uen in direct_reports.keys():
-        for i in direct_reports[supervisor_uen]:
-            people[i]["Supervisor Fraction"] = (people[i]["Total Reports"] + 1) / num_reports
+    for i in p["Direct Reports"]:
+        people[i]["Supervisor Fraction"] = (people[i]["Total Reports"] + 1) / num_reports
 
     return num_reports
+
+class SpiralOrgWidget(QtWidgets.QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.people = {}
+        self.top_level_supervisor = 0
+
+    def setPeople(self, people, top_level_supervisor):
+        self.people = people
+        self.top_level_supervisor = top_level_supervisor
+
+        # Work out how many layers deep the org goes.
+        level_count = [0] * 10
+        for i in people:
+            p = people[i]
+            depth = p["Org Depth"]
+            level_count[depth] += 1
+
+        self.max_depth = 0
+        for j in range((len(level_count) - 1), -1, -1):
+            if level_count[j] != 0:
+                self.max_depth = j
+                break
+
+        self.max_radius = 480
+        self.spacing = 20
+        self.ring_width = self.max_radius / (self.max_depth + 1)
+
+        self.setMinimumSize(2 * (self.spacing + self.max_radius),
+                            2 * (self.spacing + self.max_radius))
+        self.update()
+
+    def paintEvent(self, e):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.RenderHints.Antialiasing)
+        self.drawWidget(qp)
+        qp.end()
+
+    def recurseDrawWidget(self, painter, supervisor_uen, depth, start_angle, start_arc):
+        supervisor_person = self.people[supervisor_uen]
+
+        angle = start_angle
+        for i in supervisor_person["Direct Reports"]:
+            radius = (depth + 1) * self.ring_width
+            brush = QtGui.QBrush("lightgray")
+            painter.setBrush(brush)
+            arc = self.people[i]["Supervisor Fraction"] * start_arc
+            self.recurseDrawWidget(painter, i, depth + 1, angle, arc)
+            painter.drawPie(self.spacing + self.max_radius - radius,
+                            self.spacing + self.max_radius - radius,
+                            radius * 2, radius * 2,
+                            angle * 16, arc * 16)
+            angle += arc
+
+    def drawWidget(self, painter):
+        if len(self.people) == 0:
+            return
+
+        pen = QtGui.QPen(QtCore.Qt.GlobalColor.black, 1, QtCore.Qt.PenStyle.SolidLine)
+        painter.setPen(pen)
+
+        # Recursively draw the outer layers of the org chart.
+        self.recurseDrawWidget(painter, top_level_supervisor, 1, 0, 360)
+
+        # Then draw the inner-most node.
+        painter.drawEllipse(self.spacing + self.max_radius - self.ring_width,
+                            self.spacing + self.max_radius - self.ring_width,
+                            self.ring_width * 2, self.ring_width * 2)
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.org_widget = SpiralOrgWidget()
+        self.scroll_area = QtWidgets.QScrollArea()
+        self.scroll_area.setWidget(self.org_widget)
+        self.scroll_area.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+        self.setCentralWidget(self.scroll_area)
+
+    def setPeople(self, people, top_level_supervisor):
+        self.org_widget.setPeople(people, top_level_supervisor)
+        self.update()
+
 
 json_file_path = r'people.json'
 with open(json_file_path, encoding = 'utf-8') as user_file:
     json_data = json.load(user_file)
 
-fail, all_people, top_level_supervisor, all_reports = scan_json(json_data)
+fail, all_people, top_level_supervisor = scan_json(json_data)
 if fail:
     exit()
 
-scan_org_tree(all_people, top_level_supervisor, all_reports, 0)
-all_people[top_level_supervisor]["Supervisor Fraction"] = 0
+scan_org_tree(all_people, top_level_supervisor, 0)
+all_people[top_level_supervisor]["Supervisor Fraction"] = 1
 
-level_count = [0] * 10
-print(level_count)
-for i in all_people:
-    p = all_people[i]
-    depth = p["Org Depth"]
-    print(p["Person"]["UEN"], ":", p["Org Depth"], ":", p["Total Reports"], ":", p["Supervisor Fraction"])
-    level_count[depth] += 1
-
-for i in range(len(level_count)):
-    print(i, ":", level_count[i])
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.setPeople(all_people, top_level_supervisor)
+window.show()
+app.exec()
