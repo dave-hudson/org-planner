242ad0bb5160579d01e7109e1e15fe7015ffef62
Dave Hudson <dave-hudson@users.noreply.github.com>
2023-01-29 16:40:37 +0000

    Improve chart legends.

--- SalarySunburstOrgWidget.py
+++ SalarySunburstOrgWidget.py
@@ -2,15 +2,15 @@ import math
 from SunburstOrgWidget import SunburstOrgWidget, fx_rates
 
 salary_colours = {
-    "10000": [0x20, 0x20, 0xff],
-    "17783": [0x3c, 0x3c, 0xe4],
-    "31600": [0x58, 0x58, 0xd8],
-    "56234": [0x74, 0x74, 0xac],
-    "100000": [0x90, 0x90, 0x90],
-    "177830": [0xac, 0xac, 0x74],
-    "316000": [0xc8, 0xc8, 0x58],
-    "562340": [0xe4, 0xe4, 0x3c],
-    "1000000": [0xff, 0xff, 0x20]
+    "Below $17,782": [0x20, 0x20, 0xff],
+    "$17,783 to $31,599": [0x3c, 0x3c, 0xe4],
+    "$31,600 to $56,233": [0x58, 0x58, 0xd8],
+    "$56,234 to $99,999": [0x74, 0x74, 0xac],
+    "$100,000 to $177,829": [0x90, 0x90, 0x90],
+    "$177,830 to $315,999": [0xac, 0xac, 0x74],
+    "$316,000 to $562,339": [0xc8, 0xc8, 0x58],
+    "$562,340 to $999,999": [0xe4, 0xe4, 0x3c],
+    "$1,000,000 and above": [0xff, 0xff, 0x20]
 }
 
 class SalarySunburstOrgWidget(SunburstOrgWidget):
@@ -28,7 +28,7 @@ class SalarySunburstOrgWidget(SunburstOrgWidget):
             salary = p["Person"]["Salaries"][-1]["Salary"]
             salary_usd = salary * fx_rates[p["Person"]["Locations"][-1]["Location"]]
             log_salary_usd = 0
-            if salary > 0:
+            if salary >= 10000:
                 log_salary_usd = math.log10(salary_usd) - 4
 
             base_colour = int(0x70 * log_salary_usd)
