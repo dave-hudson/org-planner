ea7115680e997e58349737e1c4e9997abdc0fcf3
Dave Hudson <dave-hudson@users.noreply.github.com>
2023-02-15 13:56:38 +0000

    Support new FX rates.

--- SunburstOrgWidget.py
+++ SunburstOrgWidget.py
@@ -11,12 +11,21 @@ currencies = {
     "USA": ("USD", "$")
 }
 
-fx_rates = {
+old_fx_rates = {
     "UK": 1.33,
     "Ireland": 1.16,
     "India": 0.014,
     "Bulgaria": 1.16,
     "Singapore": 0.76,
+    "USA": 1.0
+}
+
+fx_rates = {
+    "UK": 1.18,
+    "Ireland": 1.0,
+    "India": 0.014,
+    "Bulgaria": 1.0,
+    "Singapore": 0.74,
     "USA": 1.0
 }
 
