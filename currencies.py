95956023ede025183dc605c8f428915fecb00644
Dave Hudson <dave-hudson@users.noreply.github.com>
2023-09-19 17:35:41 +0100

    Update FX rates.
    * Update FX rates.

--- currencies.py
+++ currencies.py
@@ -17,10 +17,10 @@ old_fx_rates = {
 }
 
 fx_rates = {
-    "UK": 1.18,
-    "Ireland": 1.0,
-    "India": 0.014,
-    "Bulgaria": 1.0,
-    "Singapore": 0.74,
+    "UK": 1.2369,
+    "Ireland": 1.083,
+    "India": 0.0123,
+    "Bulgaria": 1.083,
+    "Singapore": 0.7556,
     "USA": 1.0
 }
